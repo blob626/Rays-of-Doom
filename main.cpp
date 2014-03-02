2d411832cc1e1a20600004dd83eeabe599ebf5a8
Pieter Slabbert <blob626@gmail.com>
2014-03-02 16:28:06 +0200

    basic test of a basic mesh

--- main.cpp
+++ main.cpp
@@ -6,28 +6,17 @@
 #include "camera.h"
 #include "ray.h"
 #include "triangle.h"
+#include "mesh.h"
 
 int main(int argc, char* argv[])
 {
-  Vector3 origin;
-  Vector3 a(1, 0, 0);
-  Vector3 b(0, 1, 0);
-  Vector3 c(0, 0, 1);
-
-  std::cout << a.cross(b).normal().magnitude() << std::endl;
-
   int width = 100;
   int height = 100;
 
   Image image(width, height, 255);
   Camera camera(width, height);
 
-  Vector3 sphereCenter(0, 50, -50);
-  double radius = 10;
-
-  Triangle tri(Vector3(0, 50, -60),Vector3(0, 30, -20),Vector3(0, 70, -20));
-
-  
+  Mesh mesh("test.off");
   
   for(int row = 0; row < height; ++row)
     {
@@ -35,12 +24,11 @@ int main(int argc, char* argv[])
 	{
 	  
 	  Ray ray = camera.getRay(row,col);
-	  //	  double inter = ray.intersect(sphereCenter,radius);
-	  bool inter = ray.intersect(tri);
-	  if(inter)
+	  double inter = ray.intersect(mesh);
+	  if(inter > 0)
 	    {
 
-	      int color = 255;//floor(inter);
+	      int color = floor(inter*255);
 	      image.data[row][col] = Colour(color, color, color);
 	    }
 	}
